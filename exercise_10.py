55de3769fbdab3f06e5837739bcf735f919d192b
Katherine <bookaholic214@gmail.com>
2017-11-03 11:06:46 -0400

    Starting skeleton for Q1

--- exercise_10.py
+++ exercise_10.py
@@ -1,9 +1,23 @@
 ## exercise 10 ##
 
 # 1
+#Load packages
+import pandas
+import scipy
+import scipy.integrate as spint
+from plotnine import *
 
 
-
+#Define custom function
+def popGrowth(y,t0,r,K):
+    N=y[0]
+    dNdt=r*(1-N/K)*N
+    return [dNdt]
+params=(0.3,10)
+NO=[0.01]
+times=range(0,600)
+modelSim=spint.odeint(func=ddSim,y0=NO,t=times,args=params)
+modelOutput=pandas.DataFrame({"t":times,"N":modelSim[:,0]})
 
 
 

0aecd5983c59b89e7bc25dc8f5ecb94aff2864ad
Katherine <bookaholic214@gmail.com>
2017-11-03 11:18:56 -0400

    Added function and loop to Q1

--- exercise_10.py
+++ exercise_10.py
@@ -13,11 +13,19 @@ def popGrowth(y,t0,r,K):
     N=y[0]
     dNdt=r*(1-N/K)*N
     return [dNdt]
-params=(0.3,10)
-NO=[0.01]
 times=range(0,600)
-modelSim=spint.odeint(func=ddSim,y0=NO,t=times,args=params)
-modelOutput=pandas.DataFrame({"t":times,"N":modelSim[:,0]})
+
+#Set a pool of values for growth rate
+rK=[-.1,.1,.4,.8,1.0]
+#Dataframe for storing model output
+store_rK=pandas.DataFrame({"time":times,"r1":0,"r2":0,"r3":0,"r4":0,"r5":0})
+
+#Using a for loop to make my life easier
+for i in range(0,len(gRates)):
+    params=(rK[i],K)
+    popGrowthSim=spint.odeint(func=ddSim,y0=y0,t=times,args=params)
+    store_rK.iloc[:i]=popGrowthSim[:0]
+
 
 
 

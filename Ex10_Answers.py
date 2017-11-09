bed4719dd1955144bdf6c0660b8c6f860152ed4d
Joe C <jchambe5@nd.edu>
2017-11-08 21:37:39 -0500

    attempts made on question 2 but keep getting the same error at odeint step

--- Ex10_Answers.py
+++ Ex10_Answers.py
@@ -54,7 +54,7 @@ K=[50]
 times=range(0,500)
 
 N0s=[1,50,100] #Define a list of initial sizes for each individual population
-store_N0s=pandas.DataFrame({"time":times,"N01":0,"N02":0,"N03":0}) #Initialize a data frame to store initial sizes with columns for each
+store_N0s=pandas.DataFrame({"time":times,"N01":1,"N02":50,"N03":1500}) #Initialize a data frame to store initial sizes with columns for each
                                                                #initial size, and the times modeled
 for i in range(0,len(N0s)): #Set up a for loop that runs through each N0 in the list and models population growth using
     params=(r,K)       #other established parameters
@@ -67,4 +67,99 @@ capacity=capacity+geom_line(store_N0s,aes(y="N02"))
 capacity=capacity+geom_line(store_N0s,aes(y="N03"))
 
 capacity #Display graph
+
+# Question 2
+
+
+import pandas
+import scipy
+import scipy.integrate as spint
+from plotnine import *
+import numpy as np
+from scipy.integrate import odeint
+
+
+N=1000
+S=999
+I=1
+R=0
+T=range(0,500)
+
+B=[.0005]
+gamma=[.05]
+
+params= (N, B, gamma)
+
+def ddSim(y, t, N, B, gamma):
+    y = S, I, R
+    dSdt=(-B*I*S)
+    dIdt=(B*I*S-(gamma*I))
+    dRdt=(gamma*I)
+    return (dSdt, dIdt, dRdt)
+
+y0= S, I, R
+ret=odeint(func=ddSim,y0=N,t=T,args=params)
+S, I, R =ret.T
+
+fig = plt.figure(facecolor='w')
+ax = fig.add_subplot(111, axis_bgcolor='#dddddd', axisbelow=True)
+ax.plot(t, S/1000, 'b', alpha=0.5, lw=2, label='Susceptible')
+ax.plot(t, I/1000, 'r', alpha=0.5, lw=2, label='Infected')
+ax.plot(t, R/1000, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
+ax.set_xlabel('Time /days')
+ax.set_ylabel('Number (1000s)')
+ax.set_ylim(0,1.2)
+ax.yaxis.set_tick_params(length=0)
+ax.xaxis.set_tick_params(length=0)
+ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+legend = ax.legend()
+legend.get_frame().set_alpha(0.5)
+for spine in ('top', 'right', 'bottom', 'left'):
+    ax.spines[spine].set_visible(False)
+plt.show()
+
+
+
+incidence=(It0-It1)
+prevalence=(I/(S+I+R))
+percentAffected=((I+R)/(S+I+R))
+basicReproductionNumber=(((B(S+I+R))/gamma))
+
+
+
+#########################################################
+import scipy
+import scipy.integrate as spint
+import numpy
+import matplotlib.pyplot as plt
+
+def SIR_model(y, t, beta, gamma):
+    S, I, R = y
+    
+    dSdt=(-beta*I*S)
+    dIdt=(beta*I*S-(gamma*I))
+    dRdt=(gamma*I)
+    
+    return (dSdt, dIdt, dRdt)
+
+N=1000
+S0=999
+I0=1
+R0=0
+beta=[.0005]
+gamma0=[.05]
+t=numpy.linspace(0,500)
+
+solution=scipy.integrate.odeint(SIR_model, [S0, I0, R0], t, args=(beta, gamma))
+solution= numpy.array(solution)
+
+plt.figure(figsize=[6,4])
+plt.plot(t, solution[:, 0], label="S(t)")
+plt.plot(t, solution[:, 1], label="I(t)")
+plt.plot(t, solution[:, 2], label="R(t)")
+plt.show()
+
+
+
+
 

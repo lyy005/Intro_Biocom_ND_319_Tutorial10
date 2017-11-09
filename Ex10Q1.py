16bd80d6b274284edfa36f0783fd271549fd88ed
Phil McCown <pjlmac@gmail.com>
2017-11-09 15:25:23 -0500

    Ex10Q1 is done; Ex10Q2 is half done, I'm going over the max prevalence and max infectivity rates

--- Ex10Q1.py
+++ Ex10Q1.py
@@ -8,6 +8,7 @@ def ddSim(y,t0,r,K):
     dNdt=r*(1-N/K)*N
     return [dNdt]
 times=(range(0,1000))    
+
 rs=[-0.1,0.1,0.4,0.8,1]
 store_rs=pandas.DataFrame({"time":times,"rl":0,"r2":0,"r3":0,"r4":0,"r5":0})
 for i in range(0,len(rs)):
@@ -27,10 +28,18 @@ for i in range(0,len(ks)):
 
 ns=[1,50,100]
 store_ns=pandas.DataFrame({"time":times,"nl":0,"n2":0,"n3":0})
-#problem child is below
-for i in range(0,len()):
+for i in range(0,len(ns)):
+    N=1,50,100
     pars3=(0.1,50)
     sim3=spint.odeint(func=ddSim,y0=N[i],t=times,args=pars3)
     store_ns.iloc[:,i]=sim3[:,0]
-    
 
+#Placing models into dataframe    
+modelOutputRS=pandas.DataFrame({"t":times,"N":sim[:,0]})
+modelOutputKS=pandas.DataFrame({"t":times,"N":sim2[:,0]})
+modelOutputNS=pandas.DataFrame({"t":times,"N":sim3[:,0]})
+
+#Plot simulation outputs into ggplot
+ggplot(modelOutputKS,aes(x="t",y="N"))+geom_line()+theme_classic()
+ggplot(modelOutputRS,aes(x="t",y="N"))+geom_line()+theme_classic()
+ggplot(modelOutputNS,aes(x="t",y="N"))+geom_line()+theme_classic()

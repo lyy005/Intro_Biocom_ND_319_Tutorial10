b21910c7a684b055c708f5c09ff3452a80168f2c
ayamasaki2011 <ayamasak@nd.edu>
2017-11-09 18:03:17 -0500

    AEY: Add completed Q1 Part 3 to final answers file

--- Ex10_Answers.py
+++ Ex10_Answers.py
@@ -50,23 +50,25 @@ capacity #Display graph
 ## number 1 part 3
 
 r=0.1 #Define a set growth rate and carrying capacity
-K=[50]
+K=50
 times=range(0,500)
 
 N0s=[1,50,100] #Define a list of initial sizes for each individual population
 store_N0s=pandas.DataFrame({"time":times,"N01":1,"N02":50,"N03":1500}) #Initialize a data frame to store initial sizes with columns for each
                                                                #initial size, and the times modeled
+
 for i in range(0,len(N0s)): #Set up a for loop that runs through each N0 in the list and models population growth using
-    params=(r,K)       #other established parameters
+    params=(r,K)#other established parameters
+    N0=N0s[i] #Assign N0 a single value from the array of initial population sizes
     sim=spint.odeint(func=ddSim,y0=N0,t=times,args=params)
     store_N0s.iloc[:,i]=sim[:,0] #Store population values in the column appropriate for the current carrying capacity
 
-capacity=ggplot(store_N0s,aes(x="time",y="N"))+theme_classic()#Set up base plot for population curves
-capacity=capacity+geom_line(store_N0s,aes(y="N01"))
-capacity=capacity+geom_line(store_N0s,aes(y="N02"))
-capacity=capacity+geom_line(store_N0s,aes(y="N03"))
+initpop=ggplot(store_N0s,aes(x="time",y="N"))+theme_classic()#Set up base plot for population curves
+initpop=initpop+geom_line(store_N0s,aes(y="N01"))
+initpop=initpop+geom_line(store_N0s,aes(y="N02"))
+initpop=initpop+geom_line(store_N0s,aes(y="N03"))
 
-capacity #Display graph
+initpop #Display graph
 
 # Question 2
 

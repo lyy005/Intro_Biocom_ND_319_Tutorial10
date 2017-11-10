55665b4ffc0a4678befbfe54ea90d4875444ecf8
omegadan01 <dbruzzes@nd.edi>
2017-11-10 00:11:05 -0500

    made a billion lists and have then make a dataframe of the output

--- exercise_10_B.py
+++ exercise_10_B.py
@@ -4,6 +4,7 @@ import scipy
 import scipy.integrate as si
 from plotnine import *
 
+# function
 def SIR (y,t0,beta,gamma):
     S = y[0]
     I = y[1]
@@ -13,9 +14,11 @@ def SIR (y,t0,beta,gamma):
     dR = (gamma*I)
     return dS, dI, dR
 
+# initial conditions
 times = range(0,500)
-params = (.0005, .05) #need to change these for diff betas and gammas # need to make an arraay or dataframe
+NO = [999, 1, 0]
 
+# dataframe of gamma and beta values
 data = [{'beta' : .0005, 'gamma' : .05},
         {'beta': .005, 'gamma': .5},
         {'beta': .0001, 'gamma': .1},
@@ -25,13 +28,23 @@ data = [{'beta' : .0005, 'gamma' : .05},
         {'beta': .0001, 'gamma': .06}]
 my_data = pandas.DataFrame(data)
 
-NO = [999, 1, 0]
+mdi = []
+mdp = []
+pa = []
+ro = []
+b = []
+g = []
+
+# start big for loop here
+params = (.0005, .05) #need to change these for diff betas and gammas # need to make an arraay or dataframe
+b.append(params[0])
+g.append(params[1])
 
 infection = pandas.DataFrame({"time":times,"S":0,"I":0,"R":0})
-results = []
 
 # sim
 sim = si.odeint(func=SIR, y0=NO, t=times, args=params)
+
 # fill dataframe
 infection.iloc[:,2]=sim[:,0]
 infection.iloc[:,0]=sim[:,1]
@@ -48,7 +61,7 @@ for i in range(0,len(infection),):
         incidence = I-Iold
         daily_incidence.append(incidence)
 max_daily_incidence = max(daily_incidence)
-
+mdi.append(max_daily_incidence)
 
 # calc max daily prevalence
 daily_prev = []
@@ -59,12 +72,14 @@ for i in range(0,len(infection),):
     prev = I/(S+I+R)
     daily_prev.append(prev)
 max_daily_prev = max(daily_prev)
+mdp.append(max_daily_prev)
 
 #calc percent affected over simulation- use last time step (499)
 I= infection.iloc[499]['I']
 R= infection.iloc[499]['R']
 S= infection.iloc[499]['S']
 percent_affected = (I+R)/(S+I+R)
+pa.append(percent_affected)
 
 # basic reproduction number initial SIR
 beta = params[0]
@@ -72,9 +87,22 @@ gamma = params[1]
 I= infection.iloc[0]['I']
 R= infection.iloc[0]['R']
 S= infection.iloc[0]['S']
-Ro = (beta*(S+I+R))/gamma
+repo_number = (beta*(S+I+R))/gamma
+ro.append(repo_number)
 
 
+
+
+results = pandas.DataFrame(
+    {'beta' : b,
+     'gamma' : g,
+     'max_daily_incide' : mdi,
+     'max_daily_prev' : mdp,
+     'percent_affect' : pa,
+     'repo_num' : ro})
+
+
+print results
 #need these to fill into a list or a dataframe
 # need to put all this intoa bigger loop
 

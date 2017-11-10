ad27c5c28c2184752c905b64b82ce509d62bcec5
Joe C <jchambe5@nd.edu>
2017-11-09 20:05:41 -0500

    question 2 completed

--- Ex10_Answers.py
+++ Ex10_Answers.py
@@ -10,7 +10,7 @@ def ddSim (y,t0,r,K): #Define basic model for density-dependent growth
     
 K=100 #Define a set carrying capacity & initial population size
 N0=[10]
-times=range(0,500)
+times=range(0,100)
   
 rs=[-0.1,0.1,0.4,0.8,1.0] #Define a list of growth rates for each individual population
 store_rs=pandas.DataFrame({"time":times,"r1":0,"r2":0,"r3":0,"r4":0,"r5":0}) #Initialize a data frame to store population size, with
@@ -31,7 +31,7 @@ rates #Display the graph
 
 r=0.2 #Define a set growth rate and iitial population size
 N0=[1]
-times=range(0,500)
+times=range(0,50)
 
 Ks=[10,50,100] #Define a list of carrying capacities for each individual population
 store_Ks=pandas.DataFrame({"time":times,"K1":0,"K2":0,"K3":0}) #Initialize a data frame to store population size with columns for each
@@ -50,29 +50,27 @@ capacity #Display graph
 ## number 1 part 3
 
 r=0.1 #Define a set growth rate and carrying capacity
-K=[50]
-times=range(0,500)
+K=50
+times=range(0,100)
 
 N0s=[1,50,100] #Define a list of initial sizes for each individual population
-store_N0s=pandas.DataFrame({"time":times,"N01":1,"N02":50,"N03":1500}) #Initialize a data frame to store initial sizes with columns for each
+store_N0s=pandas.DataFrame({"time":times,"N01":0,"N02":0,"N03":0}) #Initialize a data frame to store initial sizes with columns for each
                                                                #initial size, and the times modeled
 for i in range(0,len(N0s)): #Set up a for loop that runs through each N0 in the list and models population growth using
     params=(r,K)       #other established parameters
-    sim=spint.odeint(func=ddSim,y0=N0,t=times,args=params)
+    sim=spint.odeint(func=ddSim,y0=N0s[i],t=times,args=params)
     store_N0s.iloc[:,i]=sim[:,0] #Store population values in the column appropriate for the current carrying capacity
 
-capacity=ggplot(store_N0s,aes(x="time",y="N"))+theme_classic()#Set up base plot for population curves
-capacity=capacity+geom_line(store_N0s,aes(y="N01"))
-capacity=capacity+geom_line(store_N0s,aes(y="N02"))
-capacity=capacity+geom_line(store_N0s,aes(y="N03"))
+popsize=ggplot(store_N0s,aes(x="time",y="N"))+theme_classic()#Set up base plot for population curves
+popsize=popsize+geom_line(store_N0s,aes(y="N01"))
+popsize=popsize+geom_line(store_N0s,aes(y="N02"))
+popsize=popsize+geom_line(store_N0s,aes(y="N03"))
 
-capacity #Display graph
+popsize #Display graph
 
 # Question 2
 
 #########################################################
-
-# this works but only for one beta and gamma at a time 
 
 import scipy
 import scipy.integrate as spint
@@ -80,7 +78,9 @@ import numpy
 import matplotlib.pyplot as plt
 
 def SIR_model(y, t, beta, gamma):
-    S, I, R = y
+    S = y[0]
+    I = y[1]
+    R = y[2]
     
     dSdt=(-beta*I*S)
     dIdt=(beta*I*S-(gamma*I))
@@ -88,17 +88,25 @@ def SIR_model(y, t, beta, gamma):
     
     return (dSdt, dIdt, dRdt)
 
-N=1000
+N = [999, 1, 0]
 S0=999
 I0=1
 R0=0
-beta=.0005
-gamma=.05
-t=numpy.linspace(0,500)
+beta=numpy.array([.0005, .005, .0001, .00005, .0001, .0002, .0001])
+gamma=numpy.array([.05, .5, .1, .1, .05, .05, .06])
 
-solution=scipy.integrate.odeint(SIR_model, [S0, I0, R0], t, args=(.005, .5))
-solution= numpy.array(solution)
+store_S=pandas.DataFrame({"time":times,"beta1":0,"beta2":0,"beta3":0,"beta4":0,"beta5":0,"beta6":0,"beta7":0})
+store_I=pandas.DataFrame({"time":times,"beta1":0,"beta2":0,"beta3":0,"beta4":0,"beta5":0,"beta6":0,"beta7":0})
+store_R=pandas.DataFrame({"time":times,"beta1":0,"beta2":0,"beta3":0,"beta4":0,"beta5":0,"beta6":0,"beta7":0})
 
+t=numpy.linspace(0,100)
+
+for i in range(0,len(beta)):
+    params=(beta[i],gamma[i])
+    sim=spint.odeint(func=SIR_model,y0=N,t=times,args=params)
+    store_S.iloc[:,i]=sim[:,0]
+    store_I.iloc[:,i]=sim[:,1]
+    store_R.iloc[:,i]=sim[:,2]
 
 
 plt.figure(figsize=[6,4])
@@ -107,7 +115,29 @@ plt.plot(t, solution[:, 1], label="I(t)")
 plt.plot(t, solution[:, 2], label="R(t)")
 plt.show()
 
+#max daily incidence
+mdi=np.diff(store_I.iloc[:,1])
+Imax=numpy.max(mdi)
+print("max daily incidence")
+print(Imax)
 
+#max daily prevelance
+mdp=Imax/1000
+print("max daily prevalence")
+print(mdp)
 
+#percent affected over simulation
+smallS=numpy.min(store_S.iloc[:,0])
+pas=(1/smallS)
+print("percent affected over simulation")
+print(pas)
 
-
+#basic reproduction number
+S0=999
+I0=1
+R0=0
+beta=numpy.array([.0005, .005, .0001, .00005, .0001, .0002, .0001])
+gamma=numpy.array([.05, .5, .1, .1, .05, .05, .06])
+basicReproductionNumber=(((beta*(S0+I0+R0))/gamma))
+print("BasicReproductionNumber")
+print(basicReproductionNumber)

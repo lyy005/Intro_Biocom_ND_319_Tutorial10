edfdb318a9efe3736d3874c8135166fcb06e92e3
omegadan01 <dbruzzes@nd.edi>
2017-11-10 01:04:02 -0500

    OMG IT WORKS!

--- exercise_10_B.py
+++ exercise_10_B.py
@@ -28,6 +28,7 @@ data = [{'beta' : .0005, 'gamma' : .05},
         {'beta': .0001, 'gamma': .06}]
 my_data = pandas.DataFrame(data)
 
+# make lists to hold the results
 mdi = []
 mdp = []
 pa = []
@@ -36,61 +37,63 @@ b = []
 g = []
 
 # start big for loop here
-params = (.0005, .05) #need to change these for diff betas and gammas # need to make an arraay or dataframe
-b.append(params[0])
-g.append(params[1])
+for line in range(0,len(my_data),):
+    q = my_data.iloc[line]['beta']
+    p = my_data.iloc[line]['gamma']
+    params = (q, p)
 
-infection = pandas.DataFrame({"time":times,"S":0,"I":0,"R":0})
+    b.append(params[0])
+    g.append(params[1])
 
-# sim
-sim = si.odeint(func=SIR, y0=NO, t=times, args=params)
+    infection = pandas.DataFrame({"time":times,"S":0,"I":0,"R":0})
 
-# fill dataframe
-infection.iloc[:,2]=sim[:,0]
-infection.iloc[:,0]=sim[:,1]
-infection.iloc[:,1]=sim[:,2]
+    # sim
+    sim = si.odeint(func=SIR, y0=NO, t=times, args=params)
 
-# calc max daily incidence
-daily_incidence = []
-for i in range(0,len(infection),):
-    if infection.time[i]==0:
-        continue
-    else:
+    # fill dataframe
+    infection.iloc[:,2]=sim[:,0]
+    infection.iloc[:,0]=sim[:,1]
+    infection.iloc[:,1]=sim[:,2]
+
+    # calc max daily incidence
+    daily_incidence = []
+    for i in range(0,len(infection),):
+        if infection.time[i]==0:
+            continue
+        else:
+            I = infection.iloc[i]['I']
+            Iold = infection.iloc[i-1]['I']
+            incidence = I-Iold
+            daily_incidence.append(incidence)
+    max_daily_incidence = max(daily_incidence)
+    mdi.append(max_daily_incidence)
+
+    # calc max daily prevalence
+    daily_prev = []
+    for i in range(0,len(infection),):
         I = infection.iloc[i]['I']
-        Iold = infection.iloc[i-1]['I']
-        incidence = I-Iold
-        daily_incidence.append(incidence)
-max_daily_incidence = max(daily_incidence)
-mdi.append(max_daily_incidence)
+        R = infection.iloc[i]['R']
+        S = infection.iloc[i]['S']
+        prev = I/(S+I+R)
+        daily_prev.append(prev)
+    max_daily_prev = max(daily_prev)
+    mdp.append(max_daily_prev)
 
-# calc max daily prevalence
-daily_prev = []
-for i in range(0,len(infection),):
-    I = infection.iloc[i]['I']
-    R = infection.iloc[i]['R']
-    S = infection.iloc[i]['S']
-    prev = I/(S+I+R)
-    daily_prev.append(prev)
-max_daily_prev = max(daily_prev)
-mdp.append(max_daily_prev)
+    #calc percent affected over simulation- use last time step (499)
+    I= infection.iloc[499]['I']
+    R= infection.iloc[499]['R']
+    S= infection.iloc[499]['S']
+    percent_affected = (I+R)/(S+I+R)
+    pa.append(percent_affected)
 
-#calc percent affected over simulation- use last time step (499)
-I= infection.iloc[499]['I']
-R= infection.iloc[499]['R']
-S= infection.iloc[499]['S']
-percent_affected = (I+R)/(S+I+R)
-pa.append(percent_affected)
-
-# basic reproduction number initial SIR
-beta = params[0]
-gamma = params[1]
-I= infection.iloc[0]['I']
-R= infection.iloc[0]['R']
-S= infection.iloc[0]['S']
-repo_number = (beta*(S+I+R))/gamma
-ro.append(repo_number)
-
-
+    # basic reproduction number initial SIR
+    beta = params[0]
+    gamma = params[1]
+    I= infection.iloc[0]['I']
+    R= infection.iloc[0]['R']
+    S= infection.iloc[0]['S']
+    repo_number = (beta*(S+I+R))/gamma
+    ro.append(repo_number)
 
 
 results = pandas.DataFrame(
